26921315463d05986ed290b00a3a2b5aa51b3c31
mxyffff <rua0254@naver.com>
2026-05-15 00:03:17 +0900

    first

--- src/main/java/com/likelion14/PBL_Spring/member/domian/policy/StaffSubmissionPolicy.java
+++
@@ -1,9 +0,0 @@
-package com.likelion14.PBL_Spring.member.domian.policy;
-
-public class StaffSubmissionPolicy implements SubmissionPolicy {
-
-    @Override
-    public boolean canSubmit() {
-        return false;
-    }
-}
--- src/main/java/com/likelion14/PBL_Spring/member/domian/policy/SubmissionPolicy.java
+++
@@ -1,5 +0,0 @@
-package com.likelion14.PBL_Spring.member.domian.policy;
-
-public interface SubmissionPolicy {
-    boolean canSubmit();
-}
