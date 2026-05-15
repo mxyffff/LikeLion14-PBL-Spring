ec4a643fb725add30c883d06f3fdf6fd0aff4966
mxyffff <rua0254@naver.com>
2026-05-15 18:54:54 +0900

    SpringApplicaiton 실습 코드 삭제

--- src/main/java/com/likelion14/PBL_Spring/PblSpringApplication.java
+++ src/main/java/com/likelion14/PBL_Spring/PblSpringApplication.java
@@ -8,18 +8,7 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class PblSpringApplication {
 
-	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(PblSpringApplication.class, args);
-
-		// Bean 가져오기 테스트
-		MemberService service = context.getBean(MemberService.class);
-		System.out.println("서비스 가져오기 성공: " + service);
-
-		// 보너스2: 등록된 Bean 목록 출력
-		String[] beanNames = context.getBeanDefinitionNames();
-		System.out.println("=== 등록된 Bean 목록 ===");
-		for (String name : beanNames) {
-			System.out.println("Bean: " + name);
-		}
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PblSpringApplication.class, args);
+    }
 }
